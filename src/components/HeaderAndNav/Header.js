0656bab0e0eab7873eba7b5a482726a9c7bd88a4
Dean Cook <deancook371@gmail.com>
2021-07-07 15:58:16 -0700

    Link Header and Nav components

--- src/components/HeaderAndNav/Header.js
+++ src/components/HeaderAndNav/Header.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import './Header.css';
+
+import Nav from './Nav';
 
 const Header = () => {
   return (
     <header>
       <h1>Rancid Tomatillos</h1>
+      <Nav />
     </header>
   )
 }
---
+++ src/components/HeaderAndNav/Nav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Nav.css';
+
+const Nav = () => {
+  return (
+    <nav>
+      <button className='nav-button'>Browse</button>
+      <button className='nav-button'>Nav Button</button>
+      <button className='nav-button'>Nav Button</button>
+    </nav>
+  );
+}
+
+export default Nav;
