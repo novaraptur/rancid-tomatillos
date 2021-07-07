4e6a861fdb3b2b0ecbaf5386cced92faf753b32a
Dean Cook <deancook371@gmail.com>
2021-07-07 14:49:02 -0700

    Write intial structure and component imports for App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,26 @@
+import React, { Component } from 'react';
 import logo from '../../images/logo.svg';
 import './App.css';
 
-function App() {
+import Header from '../HeaderAndNav/Header';
+import FeatMovie from '../FeatMovie/FeatMovie';
+import Movies from '../Movies/Movies';
+import movieData from '../../movieData'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: movieData
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Header />
+      <FeatMovie movies={movieData} />
+      <Movies movies={movieData}/>
+    </main>
   );
 }
 
