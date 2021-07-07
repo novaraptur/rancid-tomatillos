a61c1f9ffc4642f5407829cdda8e828d9f211792
Dean Cook <deancook371@gmail.com>
2021-07-07 16:06:34 -0700

    Update favicon and page title

--- src/components/HeaderAndNav/Header.js
+++ src/components/HeaderAndNav/Header.js
@@ -6,6 +6,7 @@ import Nav from './Nav';
 const Header = () => {
   return (
     <header>
+      <img src='https://www.growstuff.org/crops/tomatillo.svg' className='logo' />
       <h1>Rancid Tomatillos</h1>
       <Nav />
     </header>
