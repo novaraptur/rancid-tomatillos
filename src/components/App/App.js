2cfbb9aeeb5bcf36ea0362aac03ea5d9a69cde2d
Dean Cook <deancook371@gmail.com>
2021-07-07 15:53:22 -0700

    Connect Header to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import logo from '../../images/logo.svg';
 import './App.css';
 
 import Header from '../HeaderAndNav/Header';
 import FeatMovie from '../FeatMovie/FeatMovie';
 import Movies from '../Movies/Movies';
-import movieData from '../../movieData'
+import movieData from '../../movieData';
 
 class App extends Component {
   constructor(props) {
@@ -15,13 +14,16 @@ class App extends Component {
     }
   }
 
-  return (
-    <main>
-      <Header />
-      <FeatMovie movies={movieData} />
-      <Movies movies={movieData}/>
-    </main>
-  );
+  render() {
+    return (
+      <div>
+        <Header />
+        <h2>THIS DOESN'T WORK UNLESS I HAVE ANOTHER ELEMENT HERE</h2>
+      </div>
+        // <FeatMovie movies={this.state.movieData} />
+        // <Movies movies={this.state.movieData}/>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/HeaderAndNav/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Rancid Tomatillos</h1>
+    </header>
+  )
+}
+
+export default Header;
