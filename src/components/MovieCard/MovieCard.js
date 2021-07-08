8cac5ad45fbcaca53eafc35851c083bea9951a7e
Claire Fields <clairefields15@gmail.com>
2021-07-07 18:36:39 -0600

    Add prop types to MovieCard and import MovieCard.css

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,14 +1,24 @@
-import { IsConstructor } from 'es-abstract'
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import './MovieCard.css';
 
-const MovieCard= () => {
+const MovieCard = props => {
+  const { id, poster, backdrop, title, rating, releaseDate } = props;
 
   return (
-    <div>
+    <div className='movie-card'>
       <h2>Card Title</h2>
     </div>
-  )
+  );
+};
 
-}
+export default MovieCard;
 
-export default MovieCard
+MovieCard.propTypes = {
+  id: PropTypes.number,
+  poster: PropTypes.string,
+  backdrop: PropTypes.string,
+  title: PropTypes.string,
+  rating: PropTypes.number,
+  releaseDate: PropTypes.string
+};
