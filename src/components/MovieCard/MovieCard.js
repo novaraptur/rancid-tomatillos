7688f268f8eba44c94f6e1243a82cd09cf648a0e
Claire Fields <clairefields15@gmail.com>
2021-07-07 18:26:39 -0600

    Pass props to Movies and map over data to create MovieCards

---
+++ src/components/MovieCard/MovieCard.js
@@ -0,0 +1,14 @@
+import { IsConstructor } from 'es-abstract'
+import React from 'react'
+
+const MovieCard= () => {
+
+  return (
+    <div>
+      <h2>Card Title</h2>
+    </div>
+  )
+
+}
+
+export default MovieCard
---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import MovieCard from '../MovieCard/MovieCard';
+
+const Movies = ({ movies }) => {
+  const movieCards = movies.movies.map(movie => {
+    return (
+      <MovieCard
+        key={movie.id}
+        id={movie.id}
+        poster={movie.poster_path}
+        backdrop={movie.backdrop_path}
+        title={movie.title}
+        rating={movie.average_rating}
+        releaseDate={movie.release_date}
+      />
+    );
+  });
+
+  return <section className='movies-container'>{movieCards}</section>;
+};
+
+export default Movies;
