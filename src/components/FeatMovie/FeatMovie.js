436dda107b7da3da562fbc846638e51e495e5335
Dean Cook <deancook371@gmail.com>
2021-07-12 14:00:10 -0700

    Allow user to view movie info of featured movie

--- src/components/FeatMovie/FeatMovie.js
+++ src/components/FeatMovie/FeatMovie.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './FeatMovie.css';
 
-const FeatMovie = ({ props }) => {
+const FeatMovie = ({ props, updateSelectedMovie }) => {
   const movieIndex = Math.floor(Math.random() * props.movies.length);
 
   function handleClick(event) {
     event.preventDefault();
     const target = parseInt(event.target.closest('button').id);
+    updateSelectedMovie(target);
   }
 
   return (
-    <section className='featured-movie-section' id={props.movies[movieIndex].id}>
+    <button
+      className='featured-movie-section'
+      id={props.movies[movieIndex].id}
+      onClick={event => handleClick(event)}
+    >
       <img
         className='featured-movie-img'
         src={props.movies[movieIndex].backdrop_path}
@@ -20,7 +25,7 @@ const FeatMovie = ({ props }) => {
         <h2>Featured</h2>
         <h3>{props.movies[movieIndex].title}</h3>
       </div>
-    </section>
+    </button>
   );
 };
 
