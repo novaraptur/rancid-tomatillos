4b72bfaee139b77cca9d504da16afd27932145c2
Dean Cook <deancook371@gmail.com>
2021-07-16 10:26:58 -0700

    Allow user to view movie details by clicking on featured movie banner

--- src/components/FeatMovie/FeatMovie.js
+++ src/components/FeatMovie/FeatMovie.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import './FeatMovie.css';
+import { NavLink } from 'react-router-dom';
 
 const FeatMovie = ({ movies }) => {
-  const movieIndex = Math.floor(Math.random() * movies.length);
-  const { id, backdrop_path, title } = movies[movieIndex];
+  const movie = movies[Math.floor(Math.random() * movies.length)];
 
   return (
-    <button className='featured-movie-section' id={`${id}-featured`}>
-      <div className='featured'>
-        <h2>Featured Movie</h2>
-        <h3>{title}</h3>
-      </div>
-      <img
-        className='featured-movie-img'
-        src={backdrop_path}
-        alt={title + ' preview image'}
-      />
-    </button>
+    <NavLink to={`/movies/${movie.id}`} key={movie.id}>
+      <button className='featured-movie-section' id={`${movie.id}-featured`}>
+        <div className='featured'>
+          <h2>Featured Movie</h2>
+          <h3>{movie.title}</h3>
+        </div>
+        <img
+          className='featured-movie-img'
+          src={movie.backdrop_path}
+          alt={movie.title + ' preview image'}
+        />
+      </button>
+    </NavLink>
   );
 };
 
