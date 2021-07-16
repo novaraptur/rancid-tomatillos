c15bd3259503e25c134caa3d8ae99644f0c4b81a
Dean Cook <70540855+novaraptur@users.noreply.github.com>
2021-07-16 12:17:25 -0600

    Destructure id, backdrop_path, title from movie

--- src/components/FeatMovie/FeatMovie.js
+++ src/components/FeatMovie/FeatMovie.js
@@ -4,18 +4,19 @@ import { NavLink } from 'react-router-dom';
 
 const FeatMovie = ({ movies }) => {
   const movie = movies[Math.floor(Math.random() * movies.length)];
+  const { id, backdrop_path, title } = movie;
 
   return (
-    <NavLink to={`/movies/${movie.id}`} key={movie.id}>
-      <button className='featured-movie-section' id={`${movie.id}-featured`}>
+    <NavLink to={`/movies/${id}`} key={id}>
+      <button className='featured-movie-section' id={`${id}-featured`}>
         <div className='featured'>
           <h2>Featured Movie</h2>
-          <h3>{movie.title}</h3>
+          <h3>{title}</h3>
         </div>
         <img
           className='featured-movie-img'
-          src={movie.backdrop_path}
-          alt={movie.title + ' preview image'}
+          src={backdrop_path}
+          alt={title + ' preview image'}
         />
       </button>
     </NavLink>
